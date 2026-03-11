d925658f59a94652dde2fe99a63f1fcdcf9b25ea
sulabhsaluja <sulabhsaluja111@gmail.com>
2026-03-11 20:12:38 +0530

    Frontend and Backend combined

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://sales-insight-automator-k67c.onrender.com/"
+  baseURL: "https://sales-insight-automator-k67c.onrender.com/api"
 });
 
 export default API;
